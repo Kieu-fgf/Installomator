f089361da44f3afd011357b2c84c56958156196b
Kieu-fgf <Kieu.Ferrington@fivegoodfriends.com.au>
2026-08-08 12:28:06 +1000

    Add Basecamp 5 label configuration

---
+++ fragments/labels/basecamp5.sh
@@ -0,0 +1,14 @@
+basecamp5)
+    name="Basecamp"
+    type="dmg"
+
+    downloadURL="https://basecamp.com/desktop/Basecamp.dmg"
+
+    expectedTeamID="2WNYUYRS7G"
+
+    appName="Basecamp.app"
+
+    blockingProcesses=(
+        "Basecamp"
+    )
+    ;;
