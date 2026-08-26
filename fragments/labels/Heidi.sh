3e6caf8292f122141f2dae45b9f6e1749f938538
Kieu-fgf <Kieu.Ferrington@fivegoodfriends.com.au>
2026-08-26 15:01:48 +1000

    Add Heidi.sh for Heidi app download configuration

---
+++ fragments/labels/Heidi.sh
@@ -0,0 +1,6 @@
+heidiscribe)
+    name="Heidi"
+    appName="Heidi.app"
+    type="dmg"
+    downloadURL="https://cdn.crabnebula.app/download/heidi-scribe/scribe/latest/platform/dmg-aarch64"
+    ;;
