495cae967a378634a34770ec34be37bb6409ac7c
Kieu-fgf <Kieu.Ferrington@fivegoodfriends.com.au>
2026-08-31 09:42:26 +1000

    Add expectedTeamID to heidiscribe.sh

--- fragments/labels/heidiscribe.sh
+++ fragments/labels/heidiscribe.sh
@@ -3,4 +3,5 @@ heidiscribe)
     appName="Heidi.app"
     type="dmg"
     downloadURL="https://cdn.crabnebula.app/download/heidi-scribe/scribe/latest/platform/dmg-aarch64"
+    expectedTeamID="2A7SXV3WJH"
     ;;
