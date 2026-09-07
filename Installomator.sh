b656b4f9a77250b861bc3611e3956b0fc105cb88
Kieu Ferrington <kieuferrington@Kieus-MacBook-Pro-2.local>
2026-09-07 15:04:26 +1000

    Add Setapp label

--- Installomator.sh
+++ Installomator.sh
@@ -352,7 +352,7 @@ if [[ $(/usr/bin/arch) == "arm64" ]]; then
     fi
 fi
 VERSION="10.10beta"
-VERSIONDATE="2026-08-08"
+VERSIONDATE="2026-09-07"
 
 # MARK: Functions
 
---
+++ fragments/labels/setapp.sh
@@ -0,0 +1,7 @@
+setapp)
+    name="Setapp"
+    appName="Setapp.app"
+    type="zip"
+    downloadURL="https://go.setapp.com/download/client"
+    expectedTeamID="MEHY5QF425"
+    ;;
