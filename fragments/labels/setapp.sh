4657723a6e848c595c3236d3e530a1b9de891648
Kieu-fgf <Kieu.Ferrington@fivegoodfriends.com.au>
2026-09-07 14:29:36 +1000

    Update Setapp file type to 'zip'
    Changed file type from 'dmg' to 'zip' for Setapp.

--- fragments/labels/setapp.sh
+++ fragments/labels/setapp.sh
@@ -1,9 +1,7 @@
 setapp)
     name="Setapp"
     appName="Setapp.app"
-    type="dmg"
+    type="zip"
     downloadURL="https://go.setapp.com/download/client"
     expectedTeamID="MEHY5QF425"
     ;;
-
-#we need a dmg file link for this, not a zip file
